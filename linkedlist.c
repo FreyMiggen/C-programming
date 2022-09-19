4a8eb53d6b06e5c21dbf94c0308cd74c9f09458f
FreyMiggen <70058907+FreyMiggen@users.noreply.github.com>
2022-09-19 15:57:06 +0700

    Create linkedlist.c

---
+++ linkedlist.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct Node {int data; struct Node* next;}
+;
+void printList(struct Node* n){
+    while (n!=NULL){
+        printf("%d ",n->data);
+        n=n->next;
+    }
+    
+};
+
+void push(struct Node** head_ref, int new_data){
+    struct Node* new_node=(struct Node*) malloc(sizeof(struct Node));
+    new_node->data=new_data;
+    new_node->next=* head_ref;
+    * head_ref=new_node;
+};
+/* Given a node prev_node, insert a new node after the given
+prev_node */
+void insertAfter(struct Node* prev_node, int new_data){
+    if (prev_node==NULL) {
+        printf("The given node cannot be null \n");
+    }else{
+    struct Node* new_node=(struct Node* )malloc(sizeof(struct Node));
+    new_node->data=new_data;
+    /* 4. Make next of new node as next of prev_node */
+    new_node->next=prev_node->next;
+    /* 4. Make prev of new node the pre_node */
+    prev_node->next=new_node;
+}
+}
+
+int main()
+{
+    struct Node *head=NULL;
+    struct Node *second=NULL;
+    struct Node *third=NULL;
+    
+    head=(struct Node*)malloc(sizeof(struct Node));
+    second=(struct Node*)malloc(sizeof(struct Node));
+    third=(struct Node*)malloc(sizeof(struct Node));
+    
+    
+    head->data=1;
+    head->next=second;
+    
+    second->data=2;
+    second->next=third;
+    
+    third->data=3;
+    third->next=NULL;
+    
+    push(&head,4);
+    //insertAfter(second,9);
+    
+    printList(head);
+}
